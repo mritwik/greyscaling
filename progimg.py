4bb75e743d608ce8c94f2332e80b62ba0d219ea5
mritwik <ritwik1998@yahoo.com>
2018-12-19 18:36:45 +0530

    Update progimg.py

--- progimg.py
+++ progimg.py
@@ -6,14 +6,16 @@ Created on Wed Dec 19 17:45:20 2018
 @author: ritwik
 """
 
+
 import os
 import cv2 
-path="/home/ritwik/Pictures/ip/misc/"
+path=" ...input folder containing images should replace this text... "
+#NOTE: In windows, pls replace all slashes('/') with double slashes
 files=os.listdir(path)
 for name in files:
     imgpath=path+name
     img=cv2.imread(imgpath,0)
-    imgoutpath="/home/ritwik/Pictures/ip/out2/"+name
+    imgoutpath="  ...output destination folder should should replace this...  "+name
+    #NOTE: In windows, pls replace all slashes('/') with double slashes
+
     cv2.imwrite(imgoutpath,img)
-    #dfg
-    
